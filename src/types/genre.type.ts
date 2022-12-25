6de65c77ad7dc06553da02e87ba25cf0cb9d61c1
Georg <georgiy.nikitin@gmail.com>
2022-12-25 18:58:01 +0300

    Реализация команд generate и import через  stream

--- src/types/genre.type.ts
+++ src/types/genre.type.ts
@@ -1,3 +1,4 @@
+type TGenreName = 'comedy' | 'crime' | 'documentary' | 'drama' | 'horror' | 'family' | 'romance' | 'scifi' | 'thriller'
 export type Genre = {
- name: string
+ name: TGenreName
 }
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,15 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  genres: string[];
+  previewVideos: string[];
+  videos: string[];
+  actors: string[];
+  directors: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+  posters: string[];
+  backgroungImages: string[];
+  backgroungColors: string[];
+};
